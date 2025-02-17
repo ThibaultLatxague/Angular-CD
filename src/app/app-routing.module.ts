6146204c63d884c60765625764bf243fd9afe8b0
Thibault Latxague <tlatxague@iutbayonne.univ-pau.fr>
2025-02-17 09:00:55 +0100

    Ajout bouton page d'accueil vers listeCds (navigateByUrl)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ListCDComponent } from './list-cd/list-cd.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'catalog',
+    component: ListCDComponent
   }
 ];
 
