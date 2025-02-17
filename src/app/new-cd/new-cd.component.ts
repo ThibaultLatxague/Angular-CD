19f12b3f49c6c607043b2970b8ec9839ea6b75fd
Thibault Latxague <tlatxague@iutbayonne.univ-pau.fr>
2025-02-17 10:11:51 +0100

    Ajout base du formulaire reactif

--- src/app/new-cd/new-cd.component.ts
+++ src/app/new-cd/new-cd.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Form, FormBuilder, FormGroup } from '@angular/forms';
+import { CD } from '../models/cd.model';
 
 @Component({
   selector: 'app-new-cd',
@@ -6,6 +8,25 @@ import { Component } from '@angular/core';
   templateUrl: './new-cd.component.html',
   styleUrl: './new-cd.component.scss'
 })
-export class NewCDComponent {
+export class NewCDComponent implements OnInit {
+  
+  formulaire!: FormGroup;
+  currentCD!: CD;
 
+  constructor(private formBuilder: FormBuilder) {  }
+  
+  ngOnInit(): void {
+    this.formulaire = this.formBuilder.group({
+      title: [null],
+      author: [null],
+      thumbnail: [null],
+      dateDeSortie: [null],
+      quantite: [null],
+      price: [null],
+    })
+  }
+
+  formSoumis(): void {
+
+  }
 }
