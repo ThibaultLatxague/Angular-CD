5f48fd4f9079f5761316d77093e85ae67d82808e
Thibault Latxague <tlatxague@iutbayonne.univ-pau.fr>
2025-02-18 08:39:52 +0100

    Ajour fonction qui recupere l'id max des CD pour eviter doublon

--- src/app/services/cds.service.ts
+++ src/app/services/cds.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { CD } from '../models/cd.model';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -21,7 +22,14 @@ export class CdsService {
   }
 
   addCd(nouvCd: CD): Observable<CD> {
-    nouvCd.id = Math.floor(Math.random() * 1000);
-    return this.http.post<CD>('http://localhost:3000/CD', nouvCd);
+    return this.getCDs().pipe(
+      switchMap(cds =>
+      {
+      let maxId = 0;
+      cds.forEach (cd => { maxId = (cd.id > maxId ? cd.id : maxId); } );
+      nouvCd.id = maxId+1;
+      return this.http.post<CD>('http://localhost:3000/CD', nouvCd);
+      }
+    ));
   }
 }
